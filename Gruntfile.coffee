f4ff2b390f505d2fc91e0a8382453ed277cad76e
Adam Hull <adam@hmlad.com>
2013-12-18 17:04:19 -0800

    Add pagekite task

--- Gruntfile.coffee
+++ Gruntfile.coffee
@@ -8,6 +8,13 @@ module.exports = (grunt) ->
       options:
         node: true
 
+    pagekite:
+      options:
+        optfile: 'pagekite.rc'
+        # kitename: 'your-kite.pagekite.me'
+        # port: 3000
+        # secret: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
   grunt.registerTask 'test', ['jshint']
 
   grunt.registerTask 'default', ['test']
--- tasks/pagekite.js
+++ tasks/pagekite.js
@@ -10,5 +10,50 @@ var pkg = require('../package.json');
 
 module.exports = function(grunt) {
   grunt.registerTask('pagekite', pkg.description, function(task, target) {
+    var spawn = require('child_process').spawn;
+
+    var started = false,
+        done = this.async(),
+        options = this.options(),
+
+        optfile = options.optfile,
+        // -- OR ----
+        port = options.port,
+        kitename = options.kitename,
+        secret = options.secret;
+
+
+    grunt.log.writeln('Connecting to pagekite...');
+    var args = [];
+    if (kitename && port && secret) {
+      args.push('--clean', '--default');
+      args.push('--service_on=http:'+kitename+':localhost:'+port+':'+secret);
+    } else if (optfile) {
+      args.push('--clean', '--default');
+      args.push('--optfile=' + options.optfile);
+    }
+    var kite = spawn('./pagekite.py', args);
+
+    kite.stdout.on('data', function(data) {
+      if (!started && /status=0x11000/.test(data)) {
+        started = true;
+        var hostname = /bid=([\w:\.]+);/.exec(data)[1];
+        grunt.log.ok("Flying as " + hostname);
+        done();
+      } else if(/err=/.test(data)) {
+        grunt.log.error(data);
+        done(data);
+      } else {
+        grunt.verbose.writeln(data);
+      }
+    });
+
+    kite.stderr.on('data', function(data) {
+      grunt.log.error(data);
+    });
+
+    process.on('exit', function() {
+      kite.kill();
+    });
   });
 };
